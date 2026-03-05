5edbe7a9abd5e74f898bba93a6dce625516468d6
Tim Lagnese <timlag1305@gmail.com>
2026-03-04 22:08:25 -0800

    Clean up project and fix null reference exception

--- DropInMultiplayerV2/CreatePackage.ps1
+++
@@ -1,5 +0,0 @@
-$compress = @{
-	LiteralPath= ".\DropInMultiplayer.dll", ".\manifest.json", ".\icon.png", ".\README.md"
-	DestinationPath = "DropInMultiplayer.zip"
-}
-Compress-Archive -Force @compress
